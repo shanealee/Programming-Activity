cb65a41d80053c52a151d438809eed309cb2896c
rgengen <55066878+rgengen@users.noreply.github.com>
2019-10-13 12:20:53 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" type="text/css" href="styles.css" />
+ 
+<div id= "header">
+![alt text] [logo]
+# Slice of UX Student Life
+## A blog for all things related to being a UX student
+</div>
+
+title: Menu
+navigation: 
+  - section: Menu
+  - document: index_doc
+    label: Home
+  - document: about_us_doc
+    label: About Us
+  - document: blog_posts_doc
+    label: Blog Posts 
+  - document: contact_us_doc
+    label: Contact Us
